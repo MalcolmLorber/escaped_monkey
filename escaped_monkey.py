fb816b7e74e968ae45ee77f883b53510dc8a021a
Adam Freeman <adam@triazo.net>
2016-11-23 01:33:25 -0500

    Breaks out persist into separate file

--- escaped_monkey.py
+++ escaped_monkey.py
@@ -3,6 +3,8 @@
 import socket
 import sys
 
+import persist
+
 # Constants
 DEFAULTPORT = 60000
 
@@ -16,64 +18,6 @@ def sendMessage(message, peernum, peers):
     s.close()
 
 # Helper functions
-
-# Persistant state
-class WriteList(list):
-    """
-    List that persists to disk
-    Only works on things that can be represented as a string without newlines
-    """
-    def manual_init(self, peerID):
-        self.peerID = peerID
-        # TODO: load from disk on init
-
-    def append(self, value):
-        with open('history%d'%self.peerID, 'a') as f:
-            f.write(value + '\n')
-        super(WriteList, self).append(value)
-
-class Peer:
-    """
-    Just set variables in here naturally. Persisting and loading from
-    disk will be handled inside this class.
-    """
-    def __init__(self, peerID):
-        # TODO: load from disk on init
-        self.peerID = peerID
-        self.history = WriteList()
-        self.history.manual_init(peerID)
-        self._acceptedEpoch = 0
-        self._currentEpoch = 0
-        self._lastZxid = 0
-
-    # I know you can do this not 100% sure on the syntax
-    @acceptedEpoch.setter
-    def acceptedEpoch(self, value):
-        with open('acceptedEpoch%d'%self.peerID,'w') as f:
-            f.write(str(value))
-        self._acceptedEpoch = value
-    @acceptedEpoch
-    def acceptedEpoch(self):
-        return self._acceptedEpoch
-
-    @currentEpoch.setter
-    def currentEpoch(self, value):
-        with open('currentEpoch%d'%self.peerID,'w') as f:
-            f.write(str(value))
-        self._currentEpoch = value
-    @currentEpoch
-    def currentEpoch(self):
-        return self._currentEpoch
-
-    @lastZxid.setter
-    def lastZxid(self, value):
-        with open('lastZxid%d'%self.peerID,'w') as f:
-            f.write(str(value))
-        self._lastZxid = value
-    @lastZxid
-    def lastZxid(self):
-        return self._lastZxid
-
 
 # FSM States
 def leaderElection(s):
@@ -96,7 +40,7 @@ states = {"leader_election": leaderElection,
           "broadcast": broadcast}
 
 def main():
-    s = Peer()
+    s = persist.Peer()
 
     state = "leader_election"
     while True:
---
+++ persist.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+import os
+
+# Persistant state
+class WriteList(list):
+    """
+    List that persists to disk
+    Only works on things that can be represented as a string without newlines
+    """
+    def manual_init(self, peerID):
+        self.peerID = peerID
+        if os.path.isfile('history%d'%self.peerID):
+             with open('history%d'%self.peerID, 'r') as f:
+                 for l in f.read().split('\n'):
+                     super(WriteList, self).append(l)
+
+    def append(self, value):
+        with open('history%d'%self.peerID, 'a') as f:
+            f.write(value + '\n')
+        super(WriteList, self).append(value)
+
+class Peer(object):
+    """
+    Just set variables in here naturally. Persisting and loading from
+    disk will be handled inside this class.
+    """
+    def __init__(self, peerID):
+        self.peerID = peerID
+        self.history = WriteList()
+        self.history.manual_init(peerID)
+
+        # TODO: load from disk on init
+        if os.path.isfile('acceptedEpoch%d'%self.peerID):
+            with open('acceptedEpoch%d'%self.peerID,'w') as f:
+                self._acceptedEpoch = int(f.read())
+        else:
+            self._acceptedEpoch = 0
+
+        if os.path.isfile('currentEpoch%d'%self.peerID):
+            with open('currentEpoch%d'%self.peerID,'w') as f:
+                self._currentEpoch = int(f.read())
+        else:
+            self._currentEpoch = 0
+
+
+        if os.path.isfile('lastZxid%d'%self.peerID):
+            with open('lastZxid%d'%self.peerID,'w') as f:
+                self._lastZxid = int(f.read())
+        else:
+            self._lastZxid = 0
+
+    # I know you can do this not 100% sure on the syntax
+    @acceptedEpoch
+    def acceptedEpoch(self):
+        return self._acceptedEpoch
+    @acceptedEpoch.setter
+    def acceptedEpoch(self, value):
+        with open('acceptedEpoch%d'%self.peerID,'w') as f:
+            f.write(str(value))
+        self._acceptedEpoch = value
+
+    @currentEpoch
+    def currentEpoch(self):
+        return self._currentEpoch
+    @currentEpoch.setter
+    def currentEpoch(self, value):
+        with open('currentEpoch%d'%self.peerID,'w') as f:
+            f.write(str(value))
+        self._currentEpoch = value
+
+    @lastZxid
+    def lastZxid(self):
+        return self._lastZxid
+    @lastZxid.setter
+    def lastZxid(self, value):
+        with open('lastZxid%d'%self.peerID,'w') as f:
+            f.write(str(value))
+        self._lastZxid = value
